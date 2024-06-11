debe183244841a6f67ba35fb286672b1fd925418
PinaColadaMan <122341432+PinaColadaMan@users.noreply.github.com>
2024-06-11 11:28:30 +0530

    Completed pendulum arrangement

--- Pendulum/main.c
+++ Pendulum/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+pendulum(int size, int array[100])
 {
-    printf("Hello world!\n");
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            if(array[i]>array[j])
+            {
+                int temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+        }
+    }
+    printf("Output:\n");
+    for(int i=0;i<size;i+=2)
+    {
+        printf("%d\t",array[i]);
+    }
+    for(int i=size-1;i>0;i-=2)
+    {
+        printf("%d\t",array[i]);
+    }
+};
+int main()
+{    int n,arr[100];
+    printf("Array Size:\n");
+    scanf("%d",&n);
+    printf("Array Elements:\n");
+    for(int i=0;i<=n;i++)
+        scanf("%d",&arr[i]);
+    pendulum(n,arr);
     return 0;
 }
